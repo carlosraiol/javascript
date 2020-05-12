0466330be6de9c21a9ee3c3877d71953880d2466
Carlos Raiol <caremanuel@gmail.com>
2020-05-12 16:38:35 -0300

    Tratamentos e exceções

---
+++ js/objectConsole.js
@@ -0,0 +1,22 @@
+// Console
+
+//console.log('Black text');
+//console.warn('Yellow text with alert');
+console.error('Red error text');
+
+//console.trace();
+/*
+console.group('My group');
+    console.log('Info inside group');
+    console.log('More info inside group');
+console.groupEnd('My group');
+*/
+/*
+console.time('Log time');
+setTimeout(() => {
+    console.timeEnd('Log time');
+}, 2000);
+*/
+//console.table(['Carlos Raiol', 'Cloud Services']);
+//console.assert( 1 === 0, 'Ops');
+//console.log('%c styled log', 'color: blue;');
